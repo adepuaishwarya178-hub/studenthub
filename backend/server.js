7212a124901f909a62623b0ca60d50d849bf0e27
Aishwarya <adepuaishwarya178@gmail.com>
2026-09-12 14:32:16 +0530

    Fix MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected successfully! 🗄️✅");
   })
