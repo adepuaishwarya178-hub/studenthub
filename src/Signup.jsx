136538418c0cd4553e26e5a3fb93669670631340
Aishwarya <adepuaishwarya178@gmail.com>
2026-09-12 19:21:00 +0530

    Fix signup live backend URL

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -20,7 +20,7 @@ function Signup() {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/auth/signup",
+        "https://studenthub-dvdp.onrender.com/api/auth/signup",
         {
           method: "POST",
           headers: {
